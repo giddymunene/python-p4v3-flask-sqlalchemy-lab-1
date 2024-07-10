d6664d00d6244d74e703f78441228f49a26f12c6
gideon <gideonmunene0@gmail.com>
2024-07-10 14:11:29 +0300

    sqlalchemy lab 1

--- server/app.py
+++ server/app.py
@@ -21,7 +21,33 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
 
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquake = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    if earthquake:
+        body = {'count': len(earthquake), 'quakes': [earthquake.to_dict() for earthquake in earthquake]}
+        status = 200
+    else:
+        body = {'count': len(earthquake), 'quakes': [earthquake.to_dict() for earthquake in earthquake]}
+        status = 200
+
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    
